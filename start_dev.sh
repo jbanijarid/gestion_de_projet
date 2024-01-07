c3226f017eb0ce6d319da8d6b29fbcdbdeb043c0
ahmatali <72480005+ahmatali@users.noreply.github.com>
2024-01-07 23:07:49 +0100

    Update start_dev.sh

--- start_dev.sh
+++ start_dev.sh
@@ -7,12 +7,14 @@ code .
 # First process: Backend
 echo "Starting Backend..."
 cd backend
+npm i
 npm run dev &
 pid_backend=$!
 
 # Second process: Frontend
 echo "Starting Frontend..."
 cd ../frontend
+npm i
 npm run dev &
 pid_frontend=$!
 
